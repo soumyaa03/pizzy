da8a6f96c3f966bff06ef1349d0494fa4734a3e4
soumyaa03 <82571869+soumyaa03@users.noreply.github.com>
2022-04-22 16:31:40 +0530

    day 5

--- lib/services/maps.dart
+++ lib/services/maps.dart
@@ -2,28 +2,58 @@ import 'package:flutter/cupertino.dart';
 
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 String finaladdress = 'Searching adresss';
 
 class GenerateMaps extends ChangeNotifier {
-  late Position position;
+  late Position positionData;
+  late GoogleMapController googleMapController;
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
   Future getLocation() async {
-    Position positionData = await Geolocator.getCurrentPosition();
-    position = positionData;
-    print(positionData);
-    // GeoData geoData = await Geocoder2.getDataFromCoordinates(
-    //     latitude: position.latitude,
-    //     longitude: position.longitude,
-    //     googleMapApiKey: 'AIzaSyBlGSkNgo5xBoTOdZ3dkZtampcTqtEZaiw');
-    // AIzaSyBlGSkNgo5xBoTOdZ3dkZtampcTqtEZaiw <API Key>
-    //finaladdress = geoData.address;
+    positionData = await Geolocator.getCurrentPosition();
+
     List<Placemark> placemarks = await placemarkFromCoordinates(
         positionData.latitude, positionData.longitude);
     finaladdress = placemarks[0].subLocality.toString() +
         placemarks[0].street.toString() +
         placemarks[0].locality.toString();
-    print(placemarks[0].subAdministrativeArea);
 
     notifyListeners();
+  }
+
+  getMarkers(double lat, double long) {
+    MarkerId markerId = MarkerId(lat.toString() + long.toString());
+    Marker marker = Marker(
+        markerId: markerId,
+        icon: BitmapDescriptor.defaultMarker,
+        position: LatLng(lat, long),
+        infoWindow: InfoWindow(title: 'My title', snippet: 'Country Name'));
+    markers[markerId] = marker;
+  }
+
+  Widget fetchMaps() {
+    // ignore: void_checks
+
+    return GoogleMap(
+        mapType: MapType.hybrid,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        onTap: (loc) async {
+          List<Placemark> placemarks =
+              await placemarkFromCoordinates(loc.latitude, loc.longitude);
+          notifyListeners();
+          markers.isEmpty
+              ? getMarkers(loc.latitude, loc.longitude)
+              : markers.clear();
+        },
+        markers: Set<Marker>.of(markers.values),
+        initialCameraPosition: CameraPosition(
+            target: LatLng(positionData.latitude, positionData.longitude),
+            zoom: 18),
+        onMapCreated: (GoogleMapController mapcontroller) {
+          googleMapController = mapcontroller;
+          notifyListeners();
+        });
   }
 }
